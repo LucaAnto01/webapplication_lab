c6d53daf1e11d57fd92b862e5891c5b1ff081672
LucaAnto01 <luca.antognarelli@gmail.com>
2024-05-28 11:37:53 +0200

    Creation footer component

---
+++ react/film-library-webapp/src/components/PersonalFooter.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function PersonalFooter(props){
+    return(
+        <footer className="blockquote-footer">
+            <p>Web application film library</p>
+        </footer>
+    );
+}
+
+export default PersonalFooter;
