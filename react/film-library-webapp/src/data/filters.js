b56c2f32d047ab940cbdae57f5808d8b312263bc
LucaAnto01 <luca.antognarelli@gmail.com>
2024-05-28 11:38:40 +0200

    Updating filter structure

--- react/film-library-webapp/src/data/filters.js
+++ react/film-library-webapp/src/data/filters.js
@@ -1,17 +1,19 @@
+import dayjs from 'dayjs';
+
 const FILTERS = {
-    'filter-all': { label: 'All films', id: 'filter-all', filterFunction: () => true },
-    'filter-favorite': { label: 'Favorites films', id: 'filter-favorite', filterFunction: film => film.favorite },
-    'filter-best': { label: 'Best Rated', id: 'filter-best', filterFunction: film => film.rating >= 5 },
-    'filter-lastmonth': { label: 'Seen Last Month', id: 'filter-lastmonth', filterFunction: film => isSeenLastMonth(film) },
-    'filter-unseen': { label: 'Unseen', id: 'filter-unseen', filterFunction: film => film.watchDate ? false : true }
+  'all': { label: 'All', url: '/', filterFunction: () => true },
+  'favorite': { label: 'Favorites', url: '/filter/favorite', filterFunction: film => film.favorite },
+  'best': { label: 'Best Rated', url: '/filter/best', filterFunction: film => film.rating >= 5 },
+  'lastmonth': { label: 'Seen Last Month', url: '/filter/lastmonth', filterFunction: film => isSeenLastMonth(film) },
+  'unseen': { label: 'Unseen', url: '/filter/unseen', filterFunction: film => film.watchDate ? false : true }
 };
 
 const isSeenLastMonth = (film) => {
-    if ('watchDate' in film) {  // Accessing watchDate only if defined
-      const diff = film.watchDate.diff(dayjs(), 'month')
-      const isLastMonth = diff <= 0 && diff > -1;      // last month
-      return isLastMonth;
-    }
+  if ('watchDate' in film) {  // Accessing watchDate only if defined
+    const diff = film.watchDate.diff(dayjs(), 'month')
+    const isLastMonth = diff <= 0 && diff > -1;      // last month
+    return isLastMonth;
+  }
 }
 
 export default FILTERS;
